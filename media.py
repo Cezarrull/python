72ca001ba28d054e2332ba4f2eee6bb8b5dc4377
Cezar Augusto Fernandez Rull <cezar.afr@puccampinas.edu.br>
2025-02-17 10:18:17 -0300

    :wrench: adjustments media file

--- media.py
+++ media.py
@@ -1,2 +1,8 @@
 numOne = float(input("Digite a primeira nota: "))
 numTwo = float(input("Digite a segunda nota: "))
+media = (numOne + numTwo) / 2
+
+if media >= 5:
+    print("Sua nota é", media, "você está aprovado")
+else:
+    print("Sua nota é", media, "você está reprovado")
