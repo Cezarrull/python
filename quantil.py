d888b2f05fee5638447090c63925aea467892c9a
Cezar rull <cezarrull12345@gmail.com>
2025-02-27 08:59:38 -0300

    :wrench: adjustments from the quantil

--- quantil.py
+++ quantil.py
@@ -1,10 +1,46 @@
+
 number = float(input("Escolha um número entre 100 e 1000:"))
 
-if number <= 250:
-    print("Quantil 1°")
-elif number > 250 and number <= 500:
-    print("Quantil 2°")
-elif number > 500 and number <= 750:
-    print("Quantil 3°")
+#if number >= 100 and number <= 250:
+#    print("Quartil 1°")
+#if number > 250 and number <= 500:
+#    print("Quartil 2°")
+#if number > 500 and number <= 750:
+#    print("Quartil 3°")
+#if number > 750 and number <= 1000:
+#    print("Quartil 4°")
+#else:
+#    print("Número fora do intervalo")
+
+#########################################################
+
+#if numbber >= 100 and number <= 1000:
+#    if number < 250:
+#        print("Quartil 1°")
+#    else: 
+#        if number < 500:
+#            print("Quartil 2°")
+#        else:
+#            if number < 750:
+#                print("Quartil 3°")
+#            else:
+#                print("Quartil 4°")
+#else:
+#    print("Número fora do intervalo")
+#código mais ágil
+
+#########################################################
+
+if number >= 100 and number <= 1000:
+    if number < 250:
+        print("Quartil 1°")
+    elif number < 500:
+        print("Quartil 2°")
+    elif number < 750:
+        print("Quartil 3°")
+    else:
+        print("Quartil 4°")
 else:
-    print("Quantil 4°")
+    print("Número fora do intervalo")
+
+#########################################################
